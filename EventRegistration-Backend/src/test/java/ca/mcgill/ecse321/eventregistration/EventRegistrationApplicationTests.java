73a770c32f450c12f56736287aa1927b336dbd4a
Victoria Sanchez <victoriasanchez@Victorias-MacBook-Air.local>
2021-10-01 15:17:46 -0400

    Commented out tests.

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -1,13 +1,13 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EventRegistrationApplicationTests {
+//@SpringBootTest
+//class EventRegistrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
